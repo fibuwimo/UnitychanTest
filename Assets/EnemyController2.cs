98bf724ce3c811540bd627833cc7c206137dcd43
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-08-16 19:49:25 +0900

    8/16家

---
+++ Assets/EnemyController2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController2 : MonoBehaviour
+{
+    //目的地となるオブジェクトのトランスフォーム格納用
+    public Transform [] targets;
+    //エージェントとなるオブジェクトのNavMeshAgent格納用
+    public NavMeshAgent agent;
+    public int index=0;
+
+    // Use this for initialization
+    void Update()
+    {
+        //目的地となる座標を設定する
+        agent.destination = targets[index].position;
+    }
+
+    
+}
---
+++ Assets/JyunkaiEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JyunkaiEnemy : MonoBehaviour
+{
+    public EnemyController2 EneCon2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("通過");
+        if (other.gameObject.tag == "t0")
+        {
+            EneCon2.index = 1;
+        }
+        if (other.gameObject.tag == "t1")
+        {
+            EneCon2.index = 0;
+        }
+    }
+}
